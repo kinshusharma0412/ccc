c8e9264fa8f556f20d74416bbf3f601dbc7ff590
kinshu sharma <kinshusharma0412@gmail.com>
2024-08-02 01:26:17 +0530

    hhh

--- main-soojh-001.py
+++ main-soojh-001.py
@@ -1,72 +1,193 @@
-cookie="PHPSESSID=q9h1e1es54c6ch5f5bcojtq0g5;"
+cookie="""PHPSESSID=q9h1e1es54c6ch5f5bcojtq0g5;
+PHPSESSID=ivlv0ppd0h1ha30e30q26a84eg;
+PHPSESSID=p8tb8oph61tlckv5bp0nla9c4i;
+PHPSESSID=4qmvls61l0u2ieqvsmnl24e0pg;
+PHPSESSID=mq17qut648it6qus76rqmd1aec;
+PHPSESSID=d51qrhktbsjv03pvq0rjd6qnp0;
+PHPSESSID=3jjbgvg6mildisipha3drsqafl;
+PHPSESSID=70qbie76e9fko6f52l6a0k6e78;
+PHPSESSID=67ps77os0u1a840rs12kl6rbva;
+PHPSESSID=nnv3ui4pnahknjao0iju5pjiuv;
+PHPSESSID=jr02v2no2lonv96248dqk6ivgk;
+PHPSESSID=2b0965i4gdbr95nue6s2hspquh;
+PHPSESSID=0cd6o89nhrplqlm5lb498npkkm;
+PHPSESSID=loi755emge204d34mfuaq0krq1;
+PHPSESSID=fvib42kjfp5m9m8751gah2pigr;
+PHPSESSID=nr8p1pv2bjaarkikdkihef35a7;
+PHPSESSID=kqq8d459njm9ns99vnp9f956fe;
+PHPSESSID=0ctf397tlrun4553vtmmiqhrk7;
+PHPSESSID=colo3r0736rubr187011qleado;
+PHPSESSID=e7v3i26p3tsij8kddoj18otdc2;
+PHPSESSID=e2jps01029r9o53h9bmfkdcd8k;
+PHPSESSID=n6isel93oq1jl98dq045d51tt0;
+PHPSESSID=6ekq4mknom4ujbahvue8gesi71;
+PHPSESSID=nmmt0uhv7pcbcssv3f8hmiokmt;
+PHPSESSID=l37uijc57q9tg9uk36ahof73pf;
+PHPSESSID=nhokough1b4l8c28e9vmu851ch;
+PHPSESSID=ajeqrat89b8j66sim28j5mht08;
+PHPSESSID=lbhde73bfgh3k52uq87ju80t4p;
+PHPSESSID=ulr5vk12m34ivn48ipsjvnm987;
+PHPSESSID=j52tnfcmtjlbnke9etq8n357i6;
+PHPSESSID=duftfgklue9rbldhjb51actt9t;
+PHPSESSID=gb2r5coe28nehse18a6lc590ah;
+PHPSESSID=vt3j893sv8qr8s34cnev2um9gv;
+PHPSESSID=hpf61k3r6mmer978geqo06go4i;
+PHPSESSID=c97g4b939t8fp6c9829i2dqqsp;
+PHPSESSID=j3i74ivc70f6n2n11psgs1agn4;
+PHPSESSID=8e8qgb32rmi61iuvkqo7o5b5pl;
+PHPSESSID=gc9tfgkeupgr3jd0nh2a294hm9;
+PHPSESSID=1l4reate8n7r62eqi3posd944s;
+PHPSESSID=0kfbqqo2945sk0jsb9gg25mps4;
+PHPSESSID=48drht3sldd9jmeak0q6tcgp37;
+PHPSESSID=ejqq105blbi4le80436tnjfid5;
+PHPSESSID=so6083fbvr482o1ucn6r3nmd2b;
+PHPSESSID=4cc8b5he33hod6tnfv5mdmna2v;
+PHPSESSID=i043qctkeqdapu9p1ts1u7qe8s;
+PHPSESSID=du8nfod1ia54ktdvop36sbb5ts;
+PHPSESSID=2j5cfdjoi0ha8q2gbghn19ha45;
+PHPSESSID=a2u9e3dqd1qil092q931pe1vsh;
+PHPSESSID=u74atqeg4fgkr788378pimac8h;
+PHPSESSID=40ote0j2ar1q2muk73se02eesq;
+PHPSESSID=5l4861cvhapeskf86r6deft80l;
+PHPSESSID=rg7m8csi5fc22nrbn27dta1iec;
+PHPSESSID=8eof4t6vqjg62jv66ho8lvgobl;
+PHPSESSID=ktpdujgraorpe0hacq6hu31i34;
+PHPSESSID=afc9hp1h0phr53dn13kcmdgrem;
+PHPSESSID=i424uftsg2plult14usqoh0285;
+PHPSESSID=ma9chl4i7ptdahfeffeqqosso7;
+PHPSESSID=73e3hjbaicupf9skbkitkogt4g;
+PHPSESSID=3mi1rt2ag3rc7nmp9cr8mdapr1;
+PHPSESSID=htolbr3lcfi24abbqak8qprgu3;
+PHPSESSID=kr2jltpdomth56p4qtu3p20h1h;
+PHPSESSID=vkl4unlbm3naf44oins4koi03m;
+PHPSESSID=6h977vt1dqodq2pb53k2otfh7q;
+PHPSESSID=pbd08242vsu9l51b2vu6ismmpb;
+PHPSESSID=blj0t11jp8251ujcboeq67889u;
+PHPSESSID=tscgrmc3pgl5aag20kpgcro497;
+PHPSESSID=sp3vtjq0o5rlkpfsmf4fdr152b;
+PHPSESSID=nc98sp4quv65jrlro224r4u8pf;
+PHPSESSID=j1sndfknro0be246kdq9nu8r4i;
+PHPSESSID=55sso4q9hhodrhlgkdd3dbfsgi;
+PHPSESSID=il8md2u0fgc1vtnqkvgp6s0m0m;
+PHPSESSID=9b9ojnj3keoe98gt86urscphmo;
+PHPSESSID=c3vv9djlcub4hanqo7pj4pskan;
+PHPSESSID=3k513h6p1aqv91errdc63lmsnc;
+PHPSESSID=g1nitqn80tlm38dpcb6bpu9mug;
+PHPSESSID=agcfd1fq0d5r7v17alimhj7dav;
+PHPSESSID=bn0431tusgsi63po18n0a3htcp;
+PHPSESSID=sg7kngvmm9j25gq3sf245fh9pf;
+PHPSESSID=ltkusc1ee9b2hfq9dfq13h8j9o;
+PHPSESSID=pqfpt7qfrt43lumcc366faff30;
+PHPSESSID=ierer9b2a4qmiih9lbav68rpb5;
+PHPSESSID=qtf3l0lgj8increalps7ptbajf;
+PHPSESSID=crhf5kifd5hpo21i79gofqetgi;
+PHPSESSID=rm82dmt9dms2uo4gful97hf228;
+PHPSESSID=i9bjo5f1aoab1v6u320j2eb3b0;
+PHPSESSID=091o63hfpc611han3fc3vogg8i;
+PHPSESSID=2a4n5cufoludbqbntfmcpr94q7;
+PHPSESSID=d7r7rl63hpbsh0s18qnrduqdd4;
+PHPSESSID=to9j74rem9sn1ceit2qj3beqrj;
+PHPSESSID=g5ch9b4n0ci2lk0hfg191t6qmc;
+PHPSESSID=nrvcumqpu0a8h5bppp587i8rt3;
+PHPSESSID=ro3gljd5oni9el2okvtuoitktn;
+PHPSESSID=ce6l1jnk6epdlrf5prudfbsb7p;
+PHPSESSID=0jkqh4uclutmup7als4vnajusj;
+PHPSESSID=7r3196hv00ldof2fcbhs6s85c9;
+PHPSESSID=sjm6u4pc6h9l3o319hg87840te;
+PHPSESSID=4s2a95n5esm7pqvut4iunrtbrp;
+PHPSESSID=0b817vo6tpo0t2eu8j8imm0t4i;
+PHPSESSID=4c474ibmi0n2gtkhifli2vukk3;
+PHPSESSID=2el0oe8qql1v1b7sdlu0elmh7l;
+PHPSESSID=uer6l76hspldm4vhes6f7pr0ju;
+PHPSESSID=crhf5kifd5hpo21i79gofqetgi;"""
+
+#!pip install pymongo
+import dns
+
+#import dns.resolver
+#dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+#dns.resolver.default_resolver.nameservers=['8.8.8.8'] # this is a google public dns server,  use whatever dns server you like here
+# as a test, dns.resolver.query('www.google.com') should return an answer, not an exception'''
+from pymongo import MongoClient
+clientmongo=MongoClient('mongodb+srv://Kinshu04101:Qwert123@cluster0.ckcyx.mongodb.net/test?retryWrites=true&w=majority')
+#list_sub=clientmongo["my"]["cute"].insert_one({"baby":0})
+x=clientmongo["my"]["cute"].find_one({"baby":{"$type":"int"}})["baby"]
+
+all=len(cookie.split('\n'))
+z=x%all
+clientmongo["my"]["cute"].find_one_and_update({"baby":{"$type":"int"}},{"$set": {"baby":z+1}})
+list_sub=clientmongo["my"]["cute"].find_one({"baby":{"$type":"int"}})["baby"]
+
+
+print(f"[{z+1}/{all}] Cookie Running Now...")
+#open("/content/drive/MyDrive/Colab Notebooks/dbint.txt","w").write(str(z+1))
+
 import requests,time
-import streamlit as st
-#File Number 1
 from bs4 import BeautifulSoup
-#@st.cache_data
 def my():
-	if True:
-	    try:
-	        url = "https://mycutebaby.in/contest/participant/669bc381bf80d"
-	
-	        params = {
-	          'utm_medium': "w_7_24"
-	        }
-	
-	        headers = {
-	          'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-	          'Cookie': cookie
-	        }
-	
-	        response = requests.get(url, params=params, headers=headers)
-	
-	        ##st.write(response.text)
-	        soup=BeautifulSoup(response.text,"html.parser")
-	        try:
-	            s=soup.select("h4.v_info")[0].text
-	            st.write(s)
-	            print(s)
-	        except:
-	            pass
-	        s=soup.select("meta#token_key")[0]['value']
-	        st.write(s)
-	        print(s)
-	        open("db.txt","w").write(s)
-	    except:
-	        try:
-	            s="9UyjIsVR"#open("db.txt","r").read(
-	        except:
-	            s=""
-	        st.write(s)
-	    url = "https://mycutebaby.in/contest/daily_vote/"
-	
-	
-	    params = {
-	              'n': "669bc381bf80d",
-	              'captcha': "asdsdad",
-	              'key': s,
-	              'v': "Sharma tryinging"
-	            }
-	    headers = {
-	              'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-	            #  'Accept-Language': "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
-	              'Referer': "https://mycutebaby.in/contest/participant/669bc381bf80d",
-	            #  'Sec-Fetch-Dest': "empty",
-	            #  'Sec-Fetch-Mode': "cors",
-	            #  'Sec-Fetch-Site': "same-origin",
-	              'X-Requested-With': "XMLHttpRequest",
-	            #  'sec-ch-ua': ""Not-A.Brand";v="99", "Chromium";v="124"",
-	            #  'sec-ch-ua-full-version-list': ""Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"",
-	            #  'sec-ch-ua-mobile': "?1",
-	            #  'sec-ch-ua-model': ""M2102J20SI"",
-	            #  'sec-ch-ua-platform': ""Android"",
-	            #  'sec-ch-ua-platform-version': ""12.0.0"",
-	              'Cookie': cookie
-	            }
-	
-	    response = requests.get(url, params=params, headers=headers)
-	
-	    st.write(response.text)
-	    print(response.text)
-	   # time.sleep(5*60+1)
+   if True:
+     try:
+         url = "https://mycutebaby.in/contest/participant/669bc381bf80d"
+
+         params = {
+           'utm_medium': "w_7_24"
+         }
+
+         headers = {
+           'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+           'Cookie': cookie.split("\n")[z]
+         }
+
+         response = requests.get(url, params=params, headers=headers)
+
+         ##st.write(response.text)
+         soup=BeautifulSoup(response.text,"html.parser")
+         try:
+             s=soup.select("h4.v_info")[0].text
+             #st.write(s)
+             print(s)
+         except:
+             pass
+         s=soup.select("meta#token_key")[0]['value']
+         #st.write(s)
+         print(s)
+         open("db.txt","w").write(s)
+     except:
+         try:
+             s="9UyjIsVR"#open("db.txt","r").read(
+         except:
+             s=""
+         #st.write(s)
+     url = "https://mycutebaby.in/contest/daily_vote/"
+
+
+     params = {
+               'n': "669bc381bf80d",
+               'captcha': "asdsdad",
+               'key': s,
+               'v': "Sharma tryinging"
+             }
+     headers = {
+               'User-Agent': "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
+             #  'Accept-Language': "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
+               'Referer': "https://mycutebaby.in/contest/participant/669bc381bf80d",
+             #  'Sec-Fetch-Dest': "empty",
+             #  'Sec-Fetch-Mode': "cors",
+             #  'Sec-Fetch-Site': "same-origin",
+               'X-Requested-With': "XMLHttpRequest",
+             #  'sec-ch-ua': "\"Not-A.Brand\";v=\"99\", \"Chromium\";v=\"124\"",
+             #  'sec-ch-ua-full-version-list': "\"Not-A.Brand\";v=\"99.0.0.0\", \"Chromium\";v=\"124.0.6327.4\"",
+             #  'sec-ch-ua-mobile': "?1",
+             #  'sec-ch-ua-model': "\"M2102J20SI\"",
+             #  'sec-ch-ua-platform': "\"Android\"",
+             #  'sec-ch-ua-platform-version': "\"12.0.0\"",
+               'Cookie': cookie.split("\n")[z]
+             }
+
+     response = requests.get(url, params=params, headers=headers)
+
+     #st.write(response.text)
+     print(response.text)
+
 my()
+
