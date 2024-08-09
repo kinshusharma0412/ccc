952f3fb250cbc09891937a41d2fa0dc988245f27
kinshu sharma <kinshusharma0412@gmail.com>
2024-08-09 12:30:52 +0530

    hhh

--- main-soojh-001.py
+++ main-soojh-001.py
@@ -1,14 +1,25 @@
-import socket
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
+
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+
+    return session_info.request.remote_ip
+
+
 import streamlit as st
 
-## getting the hostname by socket.gethostname() method
-hostname = socket.gethostname()
-## getting the IP address using socket.gethostbyname() method
-ip_address = socket.gethostbyname(hostname)
-## printing the hostname and ip_address
-st.write(f"Hostname: {hostname}")
-st.write(f"IP Address: {ip_address}")
-import os,sys
-import time 
-time.sleep(5)
-os.system("kill 1")
+st.title("Title")
+st.markdown(f"The remote ip is {get_remote_ip()}")
