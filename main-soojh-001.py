774613030e9b914402a7c8d02a265b30a2e028cb
kinshusharma0412 <73776157+kinshusharma0412@users.noreply.github.com>
2024-09-06 02:58:00 +0530

    Add files via upload

--- main-soojh-001.py
+++ main-soojh-001.py
@@ -56,7 +56,7 @@ r2.headers.update({
 })
 if a:
     if "ip" not in st.secrets:
-    	secr=r2.get("https://main-soojh-0010.streamlit.app/api/v2/app/secrets").json()
+    	secr=r2.get("https://main-soojh-0010.streamlit.app/api/v2/app/secrets").json()["secrets"]
     	st.write(secr)
     	ress=r2.post("https://main-soojh-0010.streamlit.app/api/v2/app/secrets",data={"secrets":secr+'\nip='+ip_address})
     	st.write(ress.status_code)
